c1df8386d15f851b8a0dc79987d1d82fe93678f5
Billel1207 <billel_abbas@yahoo.fr>
2022-04-23 17:57:54 +0200

    ajout des modifications6

--- teste_git.py
+++ teste_git.py
@@ -14,3 +14,21 @@ test1 = [10, 20, 30, 40, 50]
 instance = math()
 
 print('la moyenne de la liste est {}'.format(instance.avg(test1)))
+
+class Imput():
+    def __init__(self,la_liste):
+        self.chiffre_liste = la_liste
+
+    def Moyenne(self):
+        somme_des_nombres = 0
+        Positions_des_None = []
+
+        for x in range(len(self.chiffre_liste)):
+            if self.chiffre_liste[x] is None:
+                Positions_des_None.append(x)
+            else:
+                somme_des_nombres+=self.chiffre_liste[x]
+        for x in Positions_des_None:
+            self.chiffre_liste[x] = somme_des_nombres / (len(self.chiffre_liste)-len(Positions_des_None))
+
+        return self.chiffre_liste
