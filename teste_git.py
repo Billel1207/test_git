e6d8bc14cf2555e7ed1d79441cc38fc4796ff0a9
Billel1207 <billel_abbas@yahoo.fr>
2022-04-23 17:41:10 +0200

    ajout des modifications3

--- teste_git.py
+++ teste_git.py
@@ -3,15 +3,13 @@ b=b+2
 
 a +b
 
+
 class math():
     def avg(self, list_):
         sum_ = sum(list_)
         n = len(list_)
-
         average = sum_ / n
-
         return average
-
 test1 = [10, 20, 30, 40, 50]
 instance = math()
 
