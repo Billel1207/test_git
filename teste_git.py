3fc3706c5106b6f28a5bb2cba1130f6dbe4d1fd3
Billel1207 <billel_abbas@yahoo.fr>
2022-04-23 17:31:12 +0200

    ajout des modifications2

--- teste_git.py
+++ teste_git.py
@@ -2,3 +2,17 @@ a = a + 5
 b=b+2
 
 a +b
+
+class math():
+    def avg(self, list_):
+        sum_ = sum(list_)
+        n = len(list_)
+
+        average = sum_ / n
+
+        return average
+
+test1 = [10, 20, 30, 40, 50]
+instance = math()
+
+print('la moyenne de la liste est {}'.format(instance.avg(test1)))
